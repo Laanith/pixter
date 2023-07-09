d02d5555b45529943c6662fd85ba0a691356646f
Laanith <lanithchouhan2004@gmail.com>
2023-07-09 20:56:21 +0530

    Initial Commit

--- start.py
+++ start.py
@@ -1,5 +1,50 @@
 import streamlit as st
-import script as scr
+from transformers import VisionEncoderDecoderModel
+from transformers import ViTFeatureExtractor
+from transformers import AutoTokenizer
+import torch
+from PIL import Image
+import warnings
+
+
+warnings.filterwarnings('ignore')
+
+
+model = VisionEncoderDecoderModel.from_pretrained(
+    "nlpconnect/vit-gpt2-image-captioning")
+feature_extractor = ViTFeatureExtractor.from_pretrained(
+    "nlpconnect/vit-gpt2-image-captioning")
+tokenizer = AutoTokenizer.from_pretrained(
+    "nlpconnect/vit-gpt2-image-captioning")
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+
+max_length = 16
+num_beams = 4
+gen_kwargs = {"max_length": max_length, "num_beams": num_beams}
+
+
+def predict_step(image_paths):
+    images = []
+    for image_path in image_paths:
+        i_image = Image.open(image_path)
+        if i_image.mode != "RGB":
+            i_image = i_image.convert(mode="RGB")
+
+        images.append(i_image)
+
+    pixel_values = feature_extractor(
+        images=images, return_tensors="pt").pixel_values
+    pixel_values = pixel_values.to(device)
+
+    output_ids = model.generate(pixel_values, **gen_kwargs)
+
+    preds = tokenizer.batch_decode(output_ids, skip_special_tokens=True)
+    preds = [pred.strip() for pred in preds]
+    return preds
+
 
 # st.sidebar.header('Navigation')
 st.sidebar.subheader('Navigation')
@@ -12,7 +57,7 @@ if nav == 'Home':
     img = st.file_uploader('Upload your image here')
     if img:
         st.text('Here is the image you uploaded')
-        lis = scr.predict_step([img])[0]
+        lis = predict_step([img])[0]
         st.image(img, width=200)
         st.text(lis)
 
